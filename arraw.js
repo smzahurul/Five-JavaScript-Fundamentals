e97c2920bd5e498f547cdbadef0730d6d701c58b
smzahurul <zahurul.it@gmail.com>
2022-06-13 21:21:28 +0600

    Five JavaScript Fundamentals

---
+++ arraw.js
@@ -0,0 +1,25 @@
+const numbers = [31, 35, 62, 99];
+const person1 = ['Rabbil', 'Hasan'];
+const products = [
+    { id: 1, name: 'laptop', price: 1000 },
+    { id: 2, name: 'Tablet', price: 600 },
+    { id: 3, name: 'Smart Watch', price: 200 },
+    { id: 4, name: 'Phone', price: 700 }
+];
+
+//Map
+const names = products.map(product => product.name);
+// console.log(names);
+const prices = products.map(price => price.price);
+console.log(prices);
+
+const prices2 = products.map(price => {
+    //do some work
+    return price.price;
+});
+
+
+//filter
+
+const cheaper = products.filter(price => price.price < 1000)
+console.log(cheaper);
